c310107149390327b83720a13061cee4a4085cc1
dongil618 <rlaehddlf618@gmail.com>
2022-08-15 00:25:55 +0900

    Fix : Order 클래스의 createdAt 최근 2년간 날짜로 랜덤값 변경

--- order_faker.py
+++ order_faker.py
@@ -8,8 +8,8 @@ import category_faker
 
 fake = Faker('ko_KR')
 
-def create_order_dataset(user_class_list, product_class_list, num):
 
+def create_order_dataset(user_class_list, product_class_list, num):
     order_class_list = []
 
     order_user_id_provider = DynamicProvider(
@@ -18,7 +18,6 @@ def create_order_dataset(user_class_list, product_class_list, num):
     )
 
     fake.add_provider(order_user_id_provider)
-
 
     order_product_id_provider = DynamicProvider(
         provider_name="set_product_in_order",
@@ -34,23 +33,24 @@ def create_order_dataset(user_class_list, product_class_list, num):
         product_id = product_class.product_id
         amount = fake.pyint(min_value=1, max_value=10)
         product_price = product_class.price
-        totalPrice = amount * product_price
-        createdAt = fake.iso8601()
+        total_price = amount * product_price
+        created_at = fake.date_time_between(start_date="-2y").isoformat("T", "auto")
 
-        order = Order(i, user_id, product_id, amount, totalPrice, createdAt)
+        order = Order(i, user_id, product_id, amount, total_price, created_at)
         order_class_list.append(order)
 
     return order_class_list
 
+
 if __name__ == "__main__":
-    
+
     user_class_list = user_faker.create_user_dataset(50)
 
     brand_class_list = brand_faker.create_brand_dataset(user_class_list, 100)
 
-    categroy_class_list = category_faker.create_catogory_dataset()
+    category_class_list = category_faker.create_catogory_dataset()
 
-    product_class_list = product_faker.create_product_dataset(brand_class_list, categroy_class_list, 1000)
+    product_class_list = product_faker.create_product_dataset(brand_class_list, category_class_list, 1000)
 
     order_class_list = create_order_dataset(user_class_list, product_class_list, 100)
 
