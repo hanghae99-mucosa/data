41e4619892f0adfd0f89e9219bc58afb5950d576
dongil618 <rlaehddlf618@gmail.com>
2022-08-15 13:13:08 +0900

    Fix : Product클래스 가격 비율 조정(#1)
    기존 Product클래스 가격은 1000원~10,000,000원으로 가격 구간별 비율이 없었다
    그래서 실제 데이터와의 괴리감을 줄여주기 위해 가격 구간별 비율을 조정하는 코드를 추가했다.
    1000원 ~ 100,000원은 60%
    100,000원 ~ 500,000원 30%
    500,000 ~ 1,000,000원 5%
    1,000,000원 ~ 5,000,000원 3%
    5,000,0000원 ~ 10,000,000원 2%
    로 구현했다.

--- product_faker.py
+++ product_faker.py
@@ -5,7 +5,6 @@ import user_faker
 import brand_faker
 import category_faker
 
-
 fake = Faker('ko_KR')
 
 def create_product_dataset(brand_class_list, categroy_class_list, num):
@@ -20,21 +19,33 @@ def create_product_dataset(brand_class_list, categroy_class_list, num):
     fake.add_provider(product_brand_id_provider)
 
 
-    product_categroy_id_provider = DynamicProvider(
-        provider_name="set_categroy_id_in_product",
+    product_category_id_provider = DynamicProvider(
+        provider_name="set_category_id_in_product",
         elements=categroy_class_list,
     )
 
-    fake.add_provider(product_categroy_id_provider)
+    fake.add_provider(product_category_id_provider)
 
     for i in range(1, num + 1):
         brand_class = fake.set_brand_id_in_product()
         brand_id = brand_class.brand_id
         name = fake.name()
         thumbnail = "http://localhost:8080" + fake.file_path(category='image', extension='png')
-        category_class = fake.set_categroy_id_in_product()
+        category_class = fake.set_category_id_in_product()
         category_id = category_class.category_id
-        price = fake.pyint(min_value=1000, max_value=10000000, step=100)
+
+        # 약 70% 20% 5% 3% 2% 비율로 max_value 지정
+        if i <= num * 0.6:
+            price = fake.pyint(min_value=1000, max_value=100000, step=100)
+        if num * 0.6 < i <= num * (0.6 + 0.3) + 1:
+            price = fake.pyint(min_value=100000, max_value=500000, step=1000)
+        if num * (0.6 + 0.3) + 1 < i <= num * (0.6 + 0.3 + 0.05):
+            price = fake.pyint(min_value=500000, max_value=1000000, step=1000)
+        if num * (0.6 + 0.3 + 0.05) < i <= num * (0.6 + 0.3 + 0.05 + 0.03):
+            price = fake.pyint(min_value=1000000, max_value=5000000, step=1000)
+        if num * (0.6 + 0.3 + 0.05 + 0.03) < i <= num * (0.6 + 0.3 + 0.05 + 0.03 + 0.02):
+            price = fake.pyint(min_value=5000000, max_value=10000000, step=1000)
+
         amount = fake.pyint(min_value=0, max_value=999)
         review_num = fake.pyint(min_value=0, max_value=50000)
         
@@ -47,6 +58,7 @@ def create_product_dataset(brand_class_list, categroy_class_list, num):
 
     return product_class_list
 
+
 if __name__ == "__main__":
     
     user_class_list = user_faker.create_user_dataset(50)
@@ -57,7 +69,29 @@ if __name__ == "__main__":
 
     product_class_list = create_product_dataset(brand_class_list, categroy_class_list, 1000)
 
+    range_1000_to_100000_cnt = 0
+    range_100000_to_500000_cnt = 0
+    range_500000_to_1000000_cnt = 0
+    range_1000000_to_5000000_cnt = 0
+    range_5000000_to_10000000_cnt = 0
     for product in product_class_list:
         print(product)
+        price = product.price
+        if 1000 <= price <= 100000:
+            range_1000_to_100000_cnt += 1
+        if 100000 < price <= 500000:
+            range_100000_to_500000_cnt += 1
+        if 500000 < price <= 1000000:
+            range_500000_to_1000000_cnt += 1
+        if 1000000 < price <= 5000000:
+            range_1000000_to_5000000_cnt += 1
+        if 5000000 < price <= 10000000:
+            range_5000000_to_10000000_cnt += 1
+
+    print("range_1000_to_100000_cnt : ", range_1000_to_100000_cnt)
+    print("range_100000_to_500000_cnt : ", range_100000_to_500000_cnt)
+    print("range_500000_to_1000000_cnt : ", range_500000_to_1000000_cnt)
+    print("range_1000000_to_5000000_cnt : ", range_1000000_to_5000000_cnt)
+    print("range_5000000_to_10000000_cnt : ", range_5000000_to_10000000_cnt)
 
     print(len(product_class_list))
