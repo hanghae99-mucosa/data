fac8809a032086c7dbb4d07079b85a92b88103b8
dongil618 <rlaehddlf618@gmail.com>
2022-08-18 01:24:18 +0900

    Chore : 오타 수정

--- table/Category.py
+++ table/Category.py
@@ -3,10 +3,10 @@ class Category:
     Category Class
     """
     
-    def __init__(self, category_id, category, parent_cateogry):
+    def __init__(self, category_id, category, parent_cateogory):
         self.__category_id = category_id
         self.__category = category
-        self.__parent_cateogry = parent_cateogry
+        self.__parent_cateogory = parent_cateogory
 
     @property
     def category_id(self):   
@@ -17,9 +17,9 @@ class Category:
         return self.__category
 
     @property
-    def parent_cateogry(self):   
-        return self.__parent_cateogry
+    def parent_cateogory(self):
+        return self.__parent_cateogory
 
     def __str__(self):
-        return 'Category(category_id={0}, category={1}, parent_cateogry={2})'.format(self.__category_id, self.__category, self.__parent_cateogry)
+        return 'Category(category_id={0}, category={1}, parent_cateogory={2})'.format(self.__category_id, self.__category, self.__parent_cateogory)
 
