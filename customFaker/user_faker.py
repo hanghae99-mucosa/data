49fdc9e44256f3e0e54014795489f95b55d18df1
dongil618 <rlaehddlf618@gmail.com>
2022-08-22 16:50:19 +0900

    Fix : 비밀번호 및 email 변경(#1)
    - 비밀번호도 랜덤값이었는데 고정값으로 변경하고 bcrypt로 생성함.
    - email도 랜덤값이었는데 테스트 용이하도록 test{user_id값}@test.test로 변경

--- customFaker/user_faker.py
+++ customFaker/user_faker.py
@@ -3,6 +3,7 @@ from model.User import User
 from faker import Faker
 from collections import OrderedDict
 import time
+import bcrypt
 
 fake = Faker('ko_KR')
 
@@ -22,8 +23,9 @@ class UserFaker():
 
         # num개의 User 생성
         for i in range(1, num + 1):
-            email = fake.unique.ascii_free_email()
-            password = fake.sha256()
+            email = "test" + str(i) + "@test.test"
+            print(email)
+            password = bcrypt.hashpw("test123!".encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
             # USER가 0, ADMIN 1
             role = fake.random_element(elements=OrderedDict([(0, 0.95), (1, 0.05)]))
             user = User(i, email, password, role)
@@ -33,15 +35,16 @@ class UserFaker():
 
 
 if __name__ == "__main__":
+    print("user_faker 시작!")
     user_faker = UserFaker()
-    user_class_list = user_faker.create_user_dataset(1000)
+    user_class_list = user_faker.create_user_dataset(100)
 
     user_cnt = 0
     admin_cnt = 0
-    for i in range(1000):
-        if user_class_list[i].role == 1:
+    for user_class in user_class_list:
+        if user_class.role == 1:
             admin_cnt += 1
-        if user_class_list[i].role == 0:
+        if user_class.role == 0:
             user_cnt += 1
 
     print("일반 사용자 수 : ", user_cnt)
